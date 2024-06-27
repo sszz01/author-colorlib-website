df9e800964f84493fb5c8d0effc4c9753b258e16
sszz01 <resios027@gmail.com>
2024-06-27 16:03:51 -0700

    Website Ver: 2.1. Ghostwriting sections were moved to the top, while the new book section was moved down. A new animation was introduced + minor text changes and improvements

---
+++ js/contact-me.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".questions h5 i");
+    let currentIndex = 0;
+  
+    function highlightNextQuestion() {
+      questions.forEach((question, index) => {
+        if (index === currentIndex) {
+          question.classList.add("highlighted");
+        } else {
+          question.classList.remove("highlighted");
+        }
+      });
+      currentIndex = (currentIndex + 1) % questions.length;
+    }
+  
+    setInterval(highlightNextQuestion, 5000);
+  });
+  
